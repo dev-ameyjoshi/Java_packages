15acf10f669d6bd4233d74d57d37def3de4b4710
Amey Joshi <ameyjoshi2001rj@gmail.com>
2022-10-23 18:08:02 +0530

    packages,learnt

---
+++ Amey.java
@@ -0,0 +1,7 @@
+package com.amey;
+
+public class Amey {
+        public static void main(String[] args) {
+                System.out.println(" I am class harry's main method!");
+        }
+}
---
+++ AmeyC.java
@@ -0,0 +1,10 @@
+package com.amey;
+// . is for sub packages 
+
+// javac -d . *.java is to consider all the packages for the java files with the package name 
+
+public class AmeyC {
+        public static void main(String[] args) {
+                System.out.println("I am a class from amey main method.");
+        }
+}
