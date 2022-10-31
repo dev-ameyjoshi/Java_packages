a6d6b7f6cec6d4a5f09ab8837ac6e1a0938d6ee8
Amey Joshi <ameyjoshi2001rj@gmail.com>
2022-10-31 16:26:02 +0530

    practice_problems_solved

---
+++ problem1.java
@@ -0,0 +1,24 @@
+package calc;
+
+
+class Calculator{
+        public void Calculate(int a , int b){
+                System.out.println("Result : "+ a +b);
+        }
+}
+class ScCalculator{
+        public void Calculate(int a , int b){
+                System.out.println("Result : "+ Math.sin(a +b));
+        }
+}
+class HyCalculator{
+        public void Calculate(int a , int b){
+                 System.out.println("Result : "+ a +b);
+        }
+}
+public class problem1 {
+         
+ public static void main(String[] args) {
+                System.out.println("I am a class from amey main method.");
+        }
+}
---
+++ problem2.java
@@ -0,0 +1,26 @@
+package folder.folderl1.folderl2;
+
+
+class Calculator{
+        
+        public void Calculate(int a , int b){
+                System.out.println("Result : "+ a +b);
+        }
+}
+class ScCalculator{
+        public void Calculate(int a , int b){
+                System.out.println("Result : "+ Math.sin(a +b));
+        }
+}
+class HyCalculator{
+        public void Calculate(int a , int b){
+                 System.out.println("Result : "+ a +b);
+        }
+}
+public class problem2 {
+         protected int proInt = 4;
+        int defint = 41;
+ public static void main(String[] args) {
+                System.out.println("I am a class from amey main method.");
+        }
+}
